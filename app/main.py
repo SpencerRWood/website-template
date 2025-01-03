386614be63335eccbef097b0ae912a7e56a8fbee
swood81 <swood81@gatech.edu>
2025-01-03 18:35:53 +0000

    "hello world" initialization

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(debug=True)
+
+# Mount the static directory for CSS/JS files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/test")
+def test_route():
+    return {"message": "FastAPI is working!"}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Hello World"})
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
