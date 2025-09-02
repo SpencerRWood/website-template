3972d3c6d71931dd56470059c7616c6d50999090
swood81 <swood81@gatech.edu>
2025-09-02 22:28:00 +0000

    Fixed the npm watch:css functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./**/*.html",                          // <— add this (or "./*.html" if you prefer)
     "./app/templates/**/*.{html,jinja,jinja2,j2}",
-    "./app/**/*.py",           // if you ever concatenate class strings in Python
-    "./static/**/*.js",        // if you add interactive components
-  ],
+    "./app/**/*.py",
+    "./static/**/*.js",
+],
   theme: { extend: {} },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
-        mytheme: {
-          primary: "#1e9167ff",
-          "primary-content": "#f8f9faff",
-          secondary: "#b7c3ccff",
-          "secondary-content": "#040405ff",
-          accent: "#b4daccff",
-          "accent-content": "#040405ff",
-          neutral: "#f8f9faff",
-          "neutral-content": "#040405ff",
-          "base-100": "#f2f4f7ff",
+        myTheme: {
+          "primary": "#040405",
+          "primary-content": "#f8f9fa",
+          "secondary": "#b7c3cc",
+          "secondary-content": "#040405",
+          "accent": "#040405",
+          "accent-content": "#f8f9fa",
+          "neutral": "#f8f9faff",
+          "neutral-content": "#040405",
+          "base-100": "#f2f4f7",
           "base-content": "#1e2937",
-          info: "#9333ea",
+          "info": "#9333ea",
           "info-content": "#fff",
-          success: "#22c55e",
+          "success": "#22c55e",
           "success-content": "#fff",
-          warning: "#f59e0b",
+          "warning": "#f59e0b",
           "warning-content": "#fff",
-          error: "#ef4444",
+          "error": "#ef4444",
           "error-content": "#fff",
         },
       },
     ],
-    defaultTheme: "mytheme",
+    defaultTheme: "myTheme",
   },
   // Optional: keep common gaps around even if purge misses them
   safelist: [
